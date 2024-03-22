d1905aca0dd5f09dcd1e3be5876098faf122c427
hotman521 <164437458+hotman521@users.noreply.github.com>
2024-03-22 19:22:16 -0400

    Update dszdzsdz.lua

--- dszdzsdz.lua
+++ dszdzsdz.lua
@@ -1,5 +1,4 @@
 getgenv().esp = {
-    Enabled = true,
 
     AutoStep = true, -- automatically updates the esp, you can disable this and use Player:Step() if you want to manually update them
     CharacterSize = Vector3.new(4, 5.75, 1.5),
@@ -133,7 +132,7 @@ function player:Step(delta)
 
     self:SetVisible(false)
 
-    if not check_pass or esp.Enabled == false then
+    if not check_pass then
         return
     else
         self.visible = true
@@ -188,10 +187,8 @@ function player:Step(delta)
         outline.Visible = true
         outline.Size = size
         outline.Position = position
-        outline.Filled = false
 
         inline.Visible = true
-        inline.Filled = false
         inline.Size = size
         inline.Position = position
         inline.Color = color or (self.useboxcolor and self.boxcolor) or esp.BoxColor
@@ -402,8 +399,6 @@ function player:SetVisible(bool)
         for i,v in next, self.drawings.skeleton do v.Visible = bool end
         for i,v in next, self.drawings.text do v[3].Visible = bool end
         for i,v in next, self.drawings.bar do v[3].Visible = bool; v[4].Visible = bool end
-
-        self.highlight.Enabled = bool
     end
 end
 
