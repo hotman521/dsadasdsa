3b58979e4ac1ad906bf4b734f1b8c0f9a4e068bd
hotman521 <164437458+hotman521@users.noreply.github.com>
2024-04-24 16:37:17 -0400

    Update fkljdajklfsa.lua

--- fkljdajklfsa.lua
+++ fkljdajklfsa.lua
@@ -802,7 +802,7 @@ function esp.UpdateTarget(player_instance)
 end
 
 -- // update
-game:GetService('RunService').PostSimulation:Connect(function(delta)
+RunService.PreRender:Connect(function(delta)
     if esp.AutoStep then
         for i, player in next, players do
             player:Step(delta)
